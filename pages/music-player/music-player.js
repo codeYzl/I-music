10c215d565d5c04f2552ff9a5947ea643fa9e4b3
codeYzl <1165759094@qq.com>
2023-05-22 00:39:36 +0800

    完成歌词的滚动

--- pages/music-player/music-player.js
+++ pages/music-player/music-player.js
@@ -1,20 +1,25 @@
 // pages/music-player/music-player.js
 import { getsongMenuTag } from "../../services/music"
 import {getSongDetail,getSongLyric} from "../../services/player"
-
+import {parseLyric} from "../../utils/parse-lyric"
+// import {throttle} from 'underscore'
 const audio = wx.createInnerAudioContext()
 const app = getApp()
 Page({
   data: {
     id: 0,
     currentSong:{},
-    lyric:"",
-    statusHeight:20,
+    lyricInfos: {},
+    statusBarHeight:20,
     currentPage: 0,
     contentHeight: 500,
     currentTime:0,
     durationTime:0,
-    sliderValue:0
+    sliderValue:0,
+    isplay: true,
+    currentLyricText: "",
+    currentIndex: 0 ,
+    lyricScrollTop: 0
   },
   onLoad(options) {
         const id = options.id
@@ -30,6 +35,9 @@ Page({
         //2. 请求歌词数据
         getSongLyric(id).then(res=>{
             this.setData({lyric: res.lrc})
+            const lrcString = res.lrc.lyric
+            const lyricInfos = parseLyric(lrcString)
+            this.setData({lyricInfos})
         })
         //播放当前歌曲
         audio.src = `https://music.163.com/song/media/outer/url?id=${id}.mp3`
@@ -37,14 +45,34 @@ Page({
         
         //监听播放进度条
         audio.onTimeUpdate(()=>{   
-            //1.记录当前时间
+            // 记录当前时间
             this.setData({currentTime: audio.currentTime *1000})
             const sliderValue = this.data.currentTime / this.data.durationTime*100 //进度条计算
             this.setData({sliderValue: sliderValue})
+            if(!this.data.lyricInfos.length) return
+            let index = this.data.lyricInfos.length - 1;          
+            for(let i = 0;i < this.data.lyricInfos.length;i++){
+                const info = this.data.lyricInfos[i]
+                if(info.time > audio.currentTime * 1000){
+                    index = i - 1
+                    break;
+                }
+            }
+            if(index === this.data.currentIndex) return
+            const currentLyricText = this.data.lyricInfos[index].text
+            this.setData({currentLyricText,currentIndex: index,lyricScrollTop: 35 * index})
+            audio.onWaiting(()=>{
+                audio.pause()
+            })
+            audio.onCanplay(()=>{
+                audio.play()
+            })
         })
+        //计算屏幕高度
         const info = wx.getSystemInfoSync()
         const contentHeight = info.screenHeight - info.statusBarHeight - 44
         this.setData({ contentHeight })
+        this.setData({statusBarHeight:info.statusBarHeight})
      },
    onSwiperChange(event){
         this.setData({currentPage: event.detail.current})
@@ -58,5 +86,25 @@ Page({
        })
       // console.log(event);
      //  console.log(2);
+   },
+   onSliderChange(event){
+       //得到点击的位置
+       const now = event.detail.value
+       //计算跳转的位置
+       const currentTime = now / 100 * this.data.durationTime
+       //设置进度条的时间
+       audio.seek(currentTime /1000)
+       this.setData({currentTime})
+   },
+   onPause(){
+       if(this.data.isplay){
+            audio.pause()
+            this.setData({isplay: false})
+       } else{
+           audio.play()
+           this.setData({isplay: true})
+       }
+      
+       
    }
 })
---
+++ utils/parse-lyric.js
@@ -0,0 +1,17 @@
+
+const timeRag= /\[(\d{2}):(\d{2})\.(\d{2,3})\]/
+export function parseLyric(lrcString){
+    const lyricInfos = []
+    const lyricLines = lrcString.split("\n")
+    for(const line of lyricLines){
+        const results = timeRag.exec(line)
+        if(!results) continue
+        const minute = results[1] * 60 * 1000
+        const second = results[2] * 1000
+        const msecond = results[3].length === 2 ? results[3] *10 : results[3] *1
+        const time = minute + second + msecond
+        const text = line.replace(timeRag,"")
+        lyricInfos.push({time,text})
+    }
+    return lyricInfos
+} 
